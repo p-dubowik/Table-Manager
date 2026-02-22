0e999e4e1f83cc288a2584034979d286f2d53bf6
Paweł Dubowik <p.dubowik27@gmail.com>
2026-02-22 18:44:41 +0100

    Move Table update form to a seperate component

---
+++ src/components/TableUpdateForm/TableUpdateForm.js
@@ -0,0 +1,65 @@
+import { Form, Button, Container, Row, Col, InputGroup } from "react-bootstrap";
+
+
+const TableUpdateForm = props => {
+    const tableData = props.tableData;
+
+    
+    
+
+
+    
+    return (
+        <Container>
+            <h1 className="ms-2 my-4">Table {tableData.id}</h1>
+
+            <Form>
+
+                <Row className="align-items-center">
+                    <Col xs="1">
+                        <Form.Label >Status:</Form.Label>
+                    </Col>
+                    <Col xs="2" >
+                        <Form.Select >
+                            <option >Busy</option>
+                            <option>Reserved</option>
+                            <option>Cleaning</option>
+                            <option>Free</option>
+                        </Form.Select>
+                    </Col>
+                </Row>
+
+                <Row className="align-items-center my-5">
+                    <Col xs="1">
+                        <Form.Label>People:</Form.Label>
+                    </Col>
+                    <Col xs="1">
+                        <Form.Control/>
+                    </Col>
+                    <Col xs="auto">/</Col>
+                    <Col xs="1">
+                        <Form.Control placeholder={tableData.maxPeople} disabled />
+                    </Col>
+                </Row>
+
+                <Row className="align-items-center">
+                    <Col xs="1">
+                        <Form.Label>Bill:</Form.Label>
+                    </Col>
+                    <Col xs="2">
+                        <InputGroup>
+                            <InputGroup.Text>$</InputGroup.Text>
+                            <Form.Control/>
+                        </InputGroup>
+                    </Col>
+                </Row>
+
+                <Button type="submit" className="my-4">UPDATE</Button>
+
+            </Form>
+        </Container>
+    );
+
+};
+
+export default TableUpdateForm;
